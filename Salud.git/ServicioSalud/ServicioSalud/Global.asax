742a5a42c614de5af89ca2d7e5f4086d06545c68
ToolsAvanzada <nicolas_zamorano@hotmail.com>
2014-12-07 12:52:46 -0300

    subiendo el proyecto

---
+++ Salud.git/ServicioSalud/ServicioSalud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServicioSalud.Global" Language="C#" %>
--- Salud.git/WebAutos/model/Model1.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\nicolan\Desktop\WebAutos\WebAutos\WebAutos\model\Model1.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
